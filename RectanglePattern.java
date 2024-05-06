49c087781f1567b3d3e3841412c8f9e34424dd60
Pritam Ghosh <rintughosh1aa@gmail.com>
2024-05-06 19:56:16 +0530

    Reactangle pattern

--- RectanglePattern.java
+++ RectanglePattern.java
@@ -1,8 +1,21 @@
+//Print the pattern
+// *****
+// *****
+// *****
+// *****
+
+
+
 import java.util.*;
 
 public class RectanglePattern {
 
     public static void main(String args[]){
-        
+        for(int i=1; i<=4; i++){
+            for(int j=1; j<=5; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
